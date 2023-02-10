3abb891e92e31664498ce398f761de9a3e13ded8
artur <acesan519@gmail.com>
2023-02-10 14:25:36 +0200

    wip

--- gqlserver/index.ts
+++ gqlserver/index.ts
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 import typeDefs from './graphql/typedefs.js';
 import resolvers from './graphql/resolvers.js';
 import { startStandaloneServer } from '@apollo/server/standalone';
-
+import express from 'express'
+import { inspect } from 'util'
 const MONGODB = 'mongodb://0.0.0.0:27017/final-fantasy'
 
 const server = new ApolloServer({
@@ -13,4 +14,12 @@ const server = new ApolloServer({
 
 mongoose.connect(MONGODB).then((res) => {
   startStandaloneServer(server,{listen: { port: 4000 }}).then(val=>{ console.log(val.url);})
+})
+
+const server2 = express();
+server2.get('/',(req, res)=>{
+  return `${inspect(req)}`
+})
+server2.listen(4001, ()=> {
+  console.log('server at http://localhost:4001')
 })
